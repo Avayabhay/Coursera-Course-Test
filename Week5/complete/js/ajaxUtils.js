0ab41202da3edeb5661ce3f4cbfe8a10b1ad0000
Abhay Sharma <avay.abhay@gmail.com>
2021-04-09 20:46:02 +0530

    Adding AJAX

--- Week5/complete/js/ajaxUtils.js
+++ Week5/complete/js/ajaxUtils.js
@@ -11,18 +11,26 @@
         }   
     };
 
-    ajaxUtils.sendGetRequest = function(requestURL, responseHandler){
+    ajaxUtils.sendGetRequest = function(requestURL, responseHandler, isJsonResponse){
         var request = getRequestObject();
         request.onreadystatechange = function(){
-            handleResponse(request, responseHandler);
+            handleResponse(request, responseHandler, isJsonResponse);
         };
         request.open("GET",requestURL,true);
         request.send();
     };
 
-    function handleResponse(request, responseHandler){
+    function handleResponse(request, responseHandler, isJsonResponse){
         if((request.readyState == 4) && (request.status == 200)){
-            responseHandler(request);
+            if(isJsonResponse == undefined){
+                isJsonResponse = true;
+            }
+            if(isJsonResponse){
+                responseHandler(JSON.parse(request.respnseText));
+            }
+            else{
+                responseHandler(request.responseText);
+            }
         }
     };
 
--- Week5/complete/js/script.js
+++ Week5/complete/js/script.js
@@ -1,9 +1,37 @@
 $(function () {
 
-    $("#navbarToggle").blur( function (event) {
+    $("#navbarToggle").blur(function (event) {
         var screenSize = window.innerWidth;
-        if(screenSize < 768){
+        if (screenSize < 768) {
             $("#collapsable-nav").collapse('hide');
         }
     });
 });
+(function (global) {
+    var obj = {};
+    var homeHTML ="./pageHTMLS/home.html";
+
+    var insertHTML = function (selector, html) {
+        var sel = document.querySelector(selector);
+        sel.innerHTML = html;
+    };
+
+    function showLoading(selector){
+        html = `
+        <div class="ajaxLoader">
+        <img src="./GIF/ajaxLoader.gif">
+        </div>`;
+        insertHTML(selector,html);
+    };
+
+    document.addEventListener("DOMContentLoaded", function(event){
+        showLoading("#main-content");
+        $ajaxUtils.sendGetRequest(homeHTML, function(responseText){
+            document.querySelector("#main-content").innerHTML = responseText;
+            console.log(responseText);
+        },false);
+    });
+    
+
+    global.$obj = obj;
+})(window);
